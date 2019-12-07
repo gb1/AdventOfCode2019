fd7395d48f74aa0e4a791e66372a2cc4cee23279
gb1 <Sap4dm1n>
2019-12-07 22:34:09 +0000

    day 5 part 2

--- day5/lib/day5.ex
+++ day5/lib/day5.ex
@@ -3,7 +3,7 @@ defmodule Day5 do
   Day 5: Sunny with a Chance of Asteroids
   """
 
-  @program_input 1
+  @program_input 5
 
   def part1 do
     File.read!("input")
@@ -14,11 +14,24 @@ defmodule Day5 do
     |> run_program(0)
   end
 
+  def part2 do
+    # "3,9,8,9,10,9,4,9,99,-1,8"
+    #"3,3,1107,-1,8,3,4,3,99"
+    "3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9"
+    |> String.split(",", trim: true)
+    |> Enum.map(&String.to_integer/1)
+    |> load_program()
+    |> run_program(0)
+  end
 
   def run_program(program, position) do
     opcode = program[position] |> parse_opcode()
 
-    {program, position} = execute opcode, position, program
+    {program, position} = execute(opcode, position, program)
+
+    # IO.inspect opcode
+    # IO.inspect program
+    # IO.inspect position
 
     case program[position] do
       99 -> :done
@@ -121,9 +134,207 @@ defmodule Day5 do
   end
 
   def execute([_, _, _, 0, 4], position, program) do
-    IO.inspect program[program[position + 1]]
+    IO.inspect(program[program[position + 1]])
     {program, position + 2}
   end
+
+  # Jump if true
+  def execute([_, 0, 0, 0, 5], position, program) do
+    case program[program[position + 1]] do
+      0 -> {program, position + 3}
+      _ -> {program, program[program[position + 2]]}
+    end
+  end
+
+  def execute([_, 0, 1, 0, 5], position, program) do
+    case program[position + 1] do
+      0 -> {program, position + 3}
+      _ -> {program, program[program[position + 2]]}
+    end
+  end
+
+  def execute([_, 1, 0, 0, 5], position, program) do
+    case program[program[position + 1]] do
+      0 -> {program, position + 3}
+      _ -> {program, program[position + 2]}
+    end
+  end
+
+  def execute([_, 1, 1, 0, 5], position, program) do
+    case program[position + 1] do
+      0 -> {program, position + 3}
+      _ -> {program, program[position + 2]}
+    end
+  end
+
+  # Jump if false
+  def execute([_, 0, 0, 0, 6], position, program) do
+    case program[program[position + 1]] do
+      0 -> {program, program[program[position + 2]]}
+      _ -> {program, position + 3}
+    end
+  end
+
+  def execute([_, 0, 1, 0, 6], position, program) do
+    case program[position + 1] do
+      0 -> {program, program[program[position + 2]]}
+      _ -> {program, position + 3}
+    end
+  end
+
+  def execute([_, 1, 0, 0, 6], position, program) do
+    case program[program[position + 1]] do
+      0 -> {program, program[position + 2]}
+      _ -> {program, position + 3}
+    end
+  end
+
+  def execute([_, 1, 1, 0, 6], position, program) do
+    case program[position + 1] do
+      0 -> {program, program[position + 2]}
+      _ -> {program, position + 3}
+    end
+  end
+
+  # Less than
+  def execute([_, 0, 0, 0, 7], position, program) do
+    cond do
+      program[program[position + 1]] < program[program[position + 2]] ->
+        {Map.put(
+          program,
+          program[position + 3],
+          1
+        ), position + 4}
+      true ->
+        {Map.put(
+          program,
+          program[position + 3],
+          0
+        ), position + 4}
+    end
+  end
+
+  def execute([_, 0, 1, 0, 7], position, program) do
+    cond do
+      program[position + 1] < program[program[position + 2]] ->
+        {Map.put(
+          program,
+          program[position + 3],
+          1
+        ), position + 4}
+      true ->
+        {Map.put(
+          program,
+          program[position + 3],
+          0
+        ), position + 4}
+    end
+  end
+
+  def execute([_, 1, 0, 0, 7], position, program) do
+    cond do
+      program[program[position + 1]] < program[position + 2] ->
+        {Map.put(
+          program,
+          program[position + 3],
+          1
+        ), position + 4}
+      true ->
+        {Map.put(
+          program,
+          program[position + 3],
+          0
+        ), position + 4}
+    end
+  end
+
+  def execute([_, 1, 1, 0, 7], position, program) do
+    cond do
+      program[position + 1] < program[position + 2] ->
+        {Map.put(
+          program,
+          program[position + 3],
+          1
+        ), position + 4}
+      true ->
+        {Map.put(
+          program,
+          program[position + 3],
+          0
+        ), position + 4}
+    end
+  end
+
+  # Equal
+  def execute([_, 0, 0, 0, 8], position, program) do
+    cond do
+      program[program[position + 1]] == program[program[position + 2]] ->
+        {Map.put(
+          program,
+          program[position + 3],
+          1
+        ), position + 4}
+      true ->
+        {Map.put(
+          program,
+          program[position + 3],
+          0
+        ), position + 4}
+    end
+  end
+
+  def execute([0, 0, 1, 0, 8], position, program) do
+    cond do
+      program[position + 1] == program[program[position + 2]] ->
+        {Map.put(
+          program,
+          program[position + 3],
+          1
+        ), position + 4}
+      true ->
+        {Map.put(
+          program,
+          program[position + 3],
+          0
+        ), position + 4}
+    end
+  end
+
+  def execute([_, 1, 0, 0, 8], position, program) do
+    cond do
+      program[program[position + 1]] == program[position + 2] ->
+        {Map.put(
+          program,
+          program[position + 3],
+          1
+        ), position + 4}
+      true ->
+        {Map.put(
+          program,
+          program[position + 3],
+          0
+        ), position + 4}
+    end
+  end
+
+  def execute([_, 1, 1, 0, 8], position, program) do
+    cond do
+      program[position + 1] == program[position + 2] ->
+        {Map.put(
+          program,
+          program[position + 3],
+          1
+        ), position + 4}
+      true ->
+        {Map.put(
+          program,
+          program[position + 3],
+          0
+        ), position + 4}
+    end
+  end
+
+
 
   @doc """
   Parse an integer opcode into a list for pattern matching
